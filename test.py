69154de8b204fb2c1b93569c5e992eb313d308d2
SY_Wang <a24586637@gmail.com>
2017-10-02 15:43:55 +0800

    x'mas tree
    x'mas tree

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-for i in range(10):
-    print("IloveKSLab")
+
+# coding: utf-8
+
+# In[9]:
+
+
+for a in range(5,0,-1):
+    for i in range(a//2+1):
+        print(" ",end="")
+    for j in range(6-a):
+        print(" *",end="")
+    print()
+
